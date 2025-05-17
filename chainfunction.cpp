23e750c8ff8b0a9ecc30a6f1bed7ecb0ff89941a
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2025-05-17 17:41:47 +0700

    menambahkan kelas buku dengan enkapsulasi status pinjaman

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 2 using namespace std;
+
+class buku
+4
+5 string judul;
+6
+7 public:
+8 buku setJudul(string judul)
+9 {
+10 this->judul = judul;
+11 return *this; // chain function
+12 }
+13 string getJudul((
+14 {
+15 return this->judul;
+16 }
+17
+18 } bukunya;
