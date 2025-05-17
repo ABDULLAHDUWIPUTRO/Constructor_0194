5b2233cdf8d25b0ab248030106692991e9f10d9b
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2025-05-17 18:21:28 +0700

    menambahkan implementasi metode setID() dan printALL() dalam kelas mahasiswa

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -16,6 +16,18 @@ public:
 
     static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
     static int getNim() { return nim; /*Definisi Function*/ }
-    
+
     mahasiswa(string pnama) : nama(pnama) { setID(); }
 };
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID(){
+    id = ++nim;
+}
+
+void mahasiswa::printAll(){
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
